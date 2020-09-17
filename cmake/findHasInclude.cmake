0be63db87dc01d2c232fd42dddedf2e774cfea38
權少 <ljqican@163.com>
2020-09-17 21:18:27 +0800

    添加CMake模塊

---
+++ cmake/findHasInclude.cmake
@@ -0,0 +1,11 @@
+﻿if(HasInclude_ROOT)
+    set(HasInclude_INCLUDE_DIRS ${HasInclude_ROOT}/include ${HasInclude_ROOT}/include/Any)
+    set(HasInclude_FOUND TRUE)
+    if(NOT TARGET HasInclude::HasInclude)
+      add_library(HasInclude_header_only INTERFACE)
+      add_library(HasInclude::HasInclude ALIAS HasInclude_header_only)
+      set_target_properties(HasInclude_header_only PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${HasInclude_INCLUDE_DIRS}"
+      )
+    endif()
+endif()
